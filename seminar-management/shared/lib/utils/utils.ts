314f5b1e5bd02d157671b96e4ce9ee86a48efac5
Gilles Sob <sngdtechnologies@gmail.com>
2025-01-23 00:30:44 +0100

    Add function to check if another cours have a same date and location

--- seminar-management/shared/lib/utils/utils.ts
+++ seminar-management/shared/lib/utils/utils.ts
@@ -1,1 +1,10 @@
+import { ICourse } from "@/shared/types/course.type";
+import { ITrainer } from "@/shared/types/trainer.type";
+
 export const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const checkIfAnotherCourseHaveSameDateAndLocation = (courseList: ICourse[], course: any): ICourse | undefined => {
+    return courseList.find((c) => {
+        return c.date === course.date && c.location === course.location;
+    });
+}
