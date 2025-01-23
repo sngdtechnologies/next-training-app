06bd0df5bc4d8f52d132a7ee232e5c19dbe46e59
Gilles Sob <sngdtechnologies@gmail.com>
2025-01-23 21:58:29 +0100

    Add function to suggest qualified trainer

--- seminar-management/shared/lib/utils/utils.ts
+++ seminar-management/shared/lib/utils/utils.ts
@@ -8,3 +8,14 @@ export const checkIfAnotherCourseHaveSameDateAndLocation = (courseList: ICourse[
         return c.date === course.date && c.location === course.location;
     });
 }
+
+export const suggestQualifiedTrainer = (trainers: ITrainer[], course: any): ITrainer[] => {
+    return trainers.sort((a, b) => {
+        const aIsQualified = a.location === course.location && a.trainingSubjects.includes(course.subject);
+        const bIsQualified = b.location === course.location && b.trainingSubjects.includes(course.subject);
+
+        if (aIsQualified && !bIsQualified) return -1;
+        if (!aIsQualified && bIsQualified) return 1;
+        return 0;
+    });
+}
